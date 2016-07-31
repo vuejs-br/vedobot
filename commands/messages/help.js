622aa506e48e8fa9dec8ca7c411415266e5f9e93
IgorLuíz <igorelmerico@hotmail.com>
2016-07-31 11:41:04 -0300

    Update Help message

--- commands/messages/help.js
+++ commands/messages/help.js
@@ -1,5 +1,5 @@
 
-module.exports = `Aqui esta o que eu posso fazer!
+module.exports = `Aqui esta o que eu posso fazer
 
 *Restrito para autores:*
 \t\t1 - *\`registrar <dd-mm-yyyy:meu-titulo:username-autor>\`* - Registra uma data para postagem.
@@ -10,4 +10,7 @@ module.exports = `Aqui esta o que eu posso fazer!
 \t\t1 - *\`ver-posts\`* - Visualiza a listagem de próximas postagens.
 \t\t2 - *\`ver-sugeridos\`* - Visualiza a listagem de temas sugeridos para posts.
 \t\t3 - *\`sugerir-post <:meu-titulo>\`* - Sugere um tema para um post.
-\t\t4 - *\`remover-sugestão <:meu-titulo>\`* - Remove uma sugestão de tema para post.`
+\t\t4 - *\`remover-sugestão <:meu-titulo>\`* - Remove uma sugestão de tema para post.
+
+Ainda estou em beta, mas qualquer bug ou sugestão,
+crie uma issue no meu repositório: https://github.com/vuejs-br/vedobot`
