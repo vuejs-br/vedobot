c8e8c5e854f8d6da19deadd10c18e3352b99bacd
IgorLuíz <igorelmerico@hotmail.com>
2016-08-01 12:41:41 -0300

    Update help message

--- commands/messages/help.js
+++ commands/messages/help.js
@@ -2,15 +2,15 @@
 module.exports = `Aqui esta o que eu posso fazer!
 
 *Restrito para autores:*
-\t\t1 - *\`registrar-post <dd-mm-yyyy:meu-titulo:username-autor>\`* - Registra uma data para postagem.
-\t\t2 - *\`remover-post <dd-mm-yyyy:username-autor>\`* - Apaga uma data para postagem.
-\t\t3 - *\`aceitar-sugestão <:meu-titulo>\`* - Colocar sugestão em produção.
+\t\t1 - *\`registrar-post dd-mm-yyyy:meu-titulo:username-autor\`* - Registra uma data para postagem.
+\t\t2 - *\`remover-post dd-mm-yyyy:username-autor\`* - Apaga uma data para postagem.
+\t\t3 - *\`aceitar-sugestão :meu-titulo\`* - Colocar sugestão em produção.
 
 *Qualquer pessoa:*
 \t\t1 - *\`ver-posts\`* - Visualiza a listagem de próximas postagens.
 \t\t2 - *\`posts-sugeridos\`* - Visualiza a listagem de temas sugeridos para posts.
-\t\t3 - *\`sugerir-post <:meu-titulo>\`* - Sugere um tema para um post.
-\t\t4 - *\`remover-sugestão <:meu-titulo>\`* - Remove uma sugestão de tema para post.
+\t\t3 - *\`sugerir-post :meu-titulo\`* - Sugere um tema para um post.
+\t\t4 - *\`remover-sugestão :meu-titulo\`* - Remove uma sugestão de tema para post.
 
 Ainda estou em beta, mas qualquer bug ou sugestão,
 crie uma issue no meu repositório: https://github.com/vuejs-br/vedobot`
